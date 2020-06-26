e4dd89417239b72cb33f94e22b5364cfe65cd766
vermathe1 <verma.the1@gmail.com>
2020-06-26 19:16:21 +0530

    make server side api changes for client side request

--- server/src/helper/index.js
+++ server/src/helper/index.js
@@ -1,31 +1,16 @@
 const api = require("../lib/api");
-
-// const getLeaveDetails = () => {
-//   return api.getMembers()
-//   .then(data => data
-//     )
-//   .catch(err => err);
-// }
-
 const getCombinedResult = (members, absenties) => {
-   return absenties.reduce((acc, absentInfo) => {
-		let userdetails = members.filter(mem => mem.userId === absentInfo.userId);
-		if(!(acc[userdetails[0].userId] && acc[userdetails[0].userId]['leaves'])){
-			acc[(userdetails[0].userId)] = {
-				leaves : []
-			}
-		}
-			acc[userdetails[0].userId] = {
-				info: userdetails[0],
-				leaves : [...acc[userdetails[0].userId].leaves,absentInfo]
-			}
-			return acc;
-	}, {});
+  return absenties.reduce((acc, absentInfo) => {
+    let userdetails = members.filter((mem) => mem.userId === absentInfo.userId);
+    absentInfo.name = userdetails[0].name;
+    acc.push(absentInfo);
+    return acc;
+  }, []);
 };
 
 const getLeaveDetails = () => {
-	return Promise.all([api.getMembers(), api.getAbsences()])
-	.then(([members, leaves]) =>  getCombinedResult(members, leaves))
-	.catch(err => err)
+  return Promise.all([api.getMembers(), api.getAbsences()])
+    .then(([members, leaves]) => getCombinedResult(members, leaves))
+    .catch((err) => err);
 };
 exports.getLeaveDetails = getLeaveDetails;
