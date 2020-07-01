f119730af302ef03a4aa705ed8c07ba747d3e88a
vermathe1 <verma.the1@gmail.com>
2020-07-01 10:46:36 +0530

    Rename Component

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './sections/Home';
-import { NotFound } from './sections/notfound';
+import { NotFound } from './sections/NotFound';
 
 const App = () => {
   return (
